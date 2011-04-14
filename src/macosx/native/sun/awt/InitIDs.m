b8eb0578664b6d8fdece392da0df7eab5d9eb51d
art <none@none>
2011-04-14 15:42:10 +0400

    java.awt.Label.initIDs() native stub

--- src/macosx/native/sun/awt/InitIDs.m
+++ src/macosx/native/sun/awt/InitIDs.m
@@ -138,6 +138,11 @@ JNIEXPORT void JNICALL Java_java_awt_KeyboardFocusManager_initIDs
 {
 }
 
+JNIEXPORT void JNICALL Java_java_awt_Label_initIDs
+    (JNIEnv *env, jclass cls)
+{
+}
+
 JNIEXPORT void JNICALL Java_java_awt_Menu_initIDs
     (JNIEnv *env, jclass cls)
 {
