2a1e6a65f4a8195261064beddb04a7a35f8be1ab
kevin_m_miller@apple.com <kevin_m_miller@apple.com>
2011-07-12 16:32:58 -0700

    Fixing broken build on some configurations

--- src/macosx/native/jobjc/src/generator/java/com/apple/internal/jobjc/generator/model/types/Type.java
+++ src/macosx/native/jobjc/src/generator/java/com/apple/internal/jobjc/generator/model/types/Type.java
@@ -24,8 +24,17 @@
  */
 package com.apple.internal.jobjc.generator.model.types;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.apple.internal.jobjc.generator.model.types.NType.NObject;
 import com.apple.internal.jobjc.generator.model.types.NType.NPointer;
+import com.apple.internal.jobjc.generator.model.types.NType.NPrimitive;
 import com.apple.internal.jobjc.generator.model.types.NType.NVoid;
+import com.apple.internal.jobjc.generator.model.types.NType.NStruct;
+import com.apple.internal.jobjc.generator.model.types.NType.NField;
 import com.apple.internal.jobjc.generator.utils.NTypeMerger;
 import com.apple.internal.jobjc.generator.utils.QA;
 import com.apple.internal.jobjc.generator.utils.Fp.Pair;
@@ -38,6 +47,23 @@ public class Type implements Comparable<Type>{
     final public String name;
     final public NType type32;
     final public NType type64;
+    
+    // HACK BS bug where some types have inconsistent definitions in the metadata,
+    // e.g. (l / i) in some places and just (l) (and thus (l / l)) in others.
+    // This is a mapping from the type name to a Type object with the correct definition.
+    private static final Map<String, Type> exceptions;
+    static {
+        exceptions = new HashMap<String, Type>();
+        exceptions.put("OSStatus", getType("OSStatus", new NPrimitive('l'), new NPrimitive('i'))); // (l / i) vs. (l)
+        exceptions.put("CGFloat", getType("CGFloat", new NPrimitive('f'), new NPrimitive('d'))); // (f / d) vs. (f)
+        exceptions.put("NSRect", getType("NSRect", getNSRectType(), getCGRectType())); // ({{_NSPoint}{_NSSize}} / {{CGPoint}{CGSize}}) vs. ({{_NSPoint}{_NSSize}})
+        exceptions.put("NSPoint", getType("NSPoint", getNSPointType(), getCGPointType())); // (_NSPoint / CGPoint) vs. (_NSPoint)
+        exceptions.put("NSSize", getType("NSSize", getNSSizeType(), getCGSizeType())); // (_NSSize / CGSize) vs. (_NSSize)
+        exceptions.put("NSInteger", getType("NSInteger", new NPrimitive('i'), new NPrimitive('q'))); // (i / q) vs. (i)
+        exceptions.put("NSPointArray", getType("NSPointArray", new NPointer(getNSPointType()), new NPointer(getCGPointType()))); // (^_NSPoint / ^CGPoint) vs. (^_NSPoint)
+        exceptions.put("NSMultibyteGlyphPacking", getType("NSMultibyteGlyphPacking", new NPrimitive('I'), new NPrimitive('Q'))); // (I / Q) vs. (I)
+        exceptions.put("CFTypeRef", getType("CFTypeRef", new NPointer(NVoid.inst()), new NPointer(NVoid.inst()))); // (^v, ^v) vs. (@, @)
+    }
     
     public static Type getType(final String name, final NType t32, final NType t64){
         return TypeCache.inst().pingType(new Type(name, t32, t64));
@@ -79,8 +105,7 @@ public class Type implements Comparable<Type>{
         if(a!=null && b==null) return a;
         if(a==null && b!=null) return b;
         if(QA.bothNullOrEquals(a, b)) return a;
-        // HACK BS bug where OSStatus is (l / i) in some spots and just (l) (and thus (l / l)) in others
-        if("OSStatus".equals(a.name)) return a;
+        if (exceptions.containsKey(a.name)) return exceptions.get(a.name); // HACK BS bug
         if(a.name != null && b.name != null && !a.name.equals(b.name)){
             System.out.println("Merging:");
             System.out.println("\ta.....: " + a.toString());
@@ -97,4 +122,52 @@ public class Type implements Comparable<Type>{
 
     // HACK BS bug where sometimes the name is declared as "id <A, B..." and sometimes it's "id<A,B..."
     public static String cleanName(String name){ return name == null ? null : name.replaceAll("\\s+", ""); }
+    
+    // HACK BS bug where NSRect has inconsistent definitions in the metadata
+    // Methods return NTypes created according to the correct definitions below:
+    //
+    // {_NSRect="origin"{_NSPoint="x"f"y"f}"size"{_NSSize="width"f"height"f}} *** 32-bit
+    // {CGRect="origin"{CGPoint="x"d"y"d}"size"{CGSize="width"d"height"d}} *** 64-bit
+    
+    private static NType getCGRectType() {
+        List<NField> fields = new ArrayList<NField>();
+        fields.add(new NField("origin", getCGPointType()));
+        fields.add(new NField("size", getCGSizeType()));
+        return new NStruct("CGRect", fields);
+    }
+    
+    private static NType getNSRectType() {
+        List<NField> fields = new ArrayList<NField>();
+        fields.add(new NField("origin", getNSPointType()));
+        fields.add(new NField("size", getNSSizeType()));
+        return new NStruct("_NSRect", fields);
+    }
+    
+    private static NType getCGPointType() {
+        List<NField> fields = new ArrayList<NField>();
+        fields.add(new NField("x", new NPrimitive('d')));
+        fields.add(new NField("y", new NPrimitive('d')));
+        return new NStruct("CGPoint", fields);
+    }
+    
+    private static NType getNSPointType() {
+        List<NField> fields = new ArrayList<NField>();
+        fields.add(new NField("x", new NPrimitive('f')));
+        fields.add(new NField("y", new NPrimitive('f')));
+        return new NStruct("_NSPoint", fields);
+    }
+    
+    private static NType getCGSizeType() {
+        List<NField> fields = new ArrayList<NField>();
+        fields.add(new NField("width", new NPrimitive('d')));
+        fields.add(new NField("height", new NPrimitive('d')));
+        return new NStruct("CGSize", fields);
+    }
+    
+    private static NType getNSSizeType() {
+        List<NField> fields = new ArrayList<NField>();
+        fields.add(new NField("width", new NPrimitive('f')));
+        fields.add(new NField("height", new NPrimitive('f')));
+        return new NStruct("_NSSize", fields);
+    }
 }
